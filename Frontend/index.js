26b50649c9945214a4e15b30ec99d9f28287ae76
reireynoso <reireynoso@gmail.com>
2020-01-14 11:39:08 -0500

    fetch, create, slap on dom

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+// 1. Fetch data
+// 2. Create elements for data
+// 3. Slap the created elements on the DOM
+
+const shoeList = document.querySelector('#shoe-list')
+
+function fetchData(){
+    fetch(`http://localhost:3000/shoes`)
+    .then(r => r.json())
+    .then(shoeData => shoeData.forEach(shoe => createElement(shoe)))
+}
+
+function createElement(shoe){
+    let li = document.createElement("li")
+    li.className = "list-group-item"
+    li.innerText = shoe.name
+
+    shoeList.append(li)    
+}
+fetchData()
+
+
+
